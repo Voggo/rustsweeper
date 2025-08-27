69f5bc4b6b055c878d774215e217d759b510fef6
Voggo <nielsviggostark@gmail.com>
2025-08-27 12:49:02 +0200

    Add game state handling and improve mine placement logic

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,13 @@ use crossterm::{
     {execute, queue},
 };
 
+enum GameState {
+    New,
+    Ongoing,
+    Won,
+    Lost,
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum CellState {
     Hidden,
@@ -39,11 +46,13 @@ struct Board {
     grid: Vec<CellBox>,
     width: usize,
     height: usize,
+    mines_placed: bool,
+    mines_to_place: usize,
 }
 
 impl Board {
     fn new(width: usize, height: usize) -> Board {
-        let mut grid = vec![
+        let grid = vec![
             CellBox {
                 kind: CellKind::Number(0),
                 state: CellState::Hidden,
@@ -54,15 +63,24 @@ impl Board {
             grid,
             width,
             height,
+            mines_placed: false,
+            mines_to_place: 99,
         }
     }
 
-    fn place_mines(&mut self, mine_count: usize) {
+    fn place_mines(&mut self, x_zero: isize, y_zero: isize) {
         let mut rng = rand::rng();
         let mut set_index = (0..(self.width * self.height)).collect::<Vec<usize>>();
         set_index.shuffle(&mut rng);
-        for i in 0..mine_count {
-            let idx = set_index[i];
+        for i in 0..self.mines_to_place {
+            let mut idx = set_index[i];
+            if (idx % self.width) as isize == x_zero
+                && (idx as isize - x_zero) / self.width as isize == y_zero
+            {
+                // if the random index is the same as the first clicked cell
+                // pick the next index in the shuffled list this will always be valid
+                idx = set_index[self.mines_to_place];
+            }
             self.grid[idx].kind = CellKind::Mine;
             let x = (idx % self.width) as isize;
             let y = (idx as isize - x) / self.width as isize;
@@ -98,59 +116,99 @@ impl Board {
             None
         }
     }
-
-    fn render_game_board(&self, stdout: &mut Stdout) -> anyhow::Result<()> {
-        let (board_start_x, board_start_y) = self.get_board_start_pos();
-        queue!(
-            stdout,
-            Clear(terminal::ClearType::All),
-            MoveTo(board_start_x, board_start_y)
-        )?;
-        let mut i = 0;
-        for y in 0..self.width {
-            for x in 0..self.height as isize {
-                if let Some(cell) = self.get_cell(x as isize, y as isize) {
-                    let (symbol, color) = match cell.state {
-                        CellState::Hidden => ("■", Color::DarkGrey), // Hidden cell
-                        CellState::Revealed => match cell.kind {
-                            CellKind::Mine => ("💣", Color::Black),      // Mine
-                            CellKind::Number(0) => ("  ", Color::Black), // Empty cell
-                            CellKind::Number(n) => (&n.to_string()[..], Color::Blue),
-                        },
-                        CellState::Flagged => ("F", Color::DarkRed), // Flagged cell
-                    };
-                    let symbol = if symbol == "💣" {
-                        format!("{:>1}", symbol)
-                    } else {
-                        format!("{:^2}", symbol)
-                    };
-                    queue!(stdout, SetForegroundColor(color), Print(symbol))?;
-                }
-            }
-            i += 1;
-            queue!(stdout, MoveTo(board_start_x, i + board_start_y))?;
-        }
-        stdout.flush()?;
-        Ok(())
-    }
     fn get_board_start_pos(&self) -> (u16, u16) {
         let (cols, rows) = crossterm::terminal::size().expect("Failed to get terminal size");
         let board_start_x = (cols as i16 - (self.width * 2) as i16) / 2;
         let board_start_y = (rows as i16 - self.height as i16) / 2;
         (board_start_x as u16, board_start_y as u16)
     }
-    fn handle_mouse_left(&mut self, event: event::MouseEvent) {
+    fn reveal_adjacent_empty(&mut self, x: isize, y: isize) -> Option<GameState> {
+        let mut to_reveal = vec![(x, y)];
+        while let Some((cx, cy)) = to_reveal.pop() {
+            if let Some(cell) = self.get_cell_mut(cx, cy) {
+                match cell.state {
+                    CellState::Revealed | CellState::Flagged => continue, // Skip if already revealed or flagged
+                    CellState::Hidden => {
+                        cell.state = CellState::Revealed;
+                        if cell.kind == CellKind::Mine {
+                            return Some(GameState::Lost); // Stop if it's a mine
+                        }
+                    }
+                }
+                if cell.kind != CellKind::Number(0) {
+                    continue; // Stop if it's not an empty cell
+                }
+                for dy in -1..=1 {
+                    for dx in -1..=1 {
+                        if dx == 0 && dy == 0 {
+                            continue;
+                        }
+                        if let Some(neighbor) = self.get_cell_mut(cx + dx, cy + dy) {
+                            match neighbor.kind {
+                                CellKind::Number(_) => {
+                                    to_reveal.push((cx + dx, cy + dy));
+                                }
+                                _ => (), // Do nothing for mines
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return None;
+    }
+    fn reveal_non_flagged(&mut self, x: isize, y: isize) -> Option<GameState> {
+        let mut ret = None;
+        for dy in -1..=1 {
+            for dx in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+                if let Some(neighbor) = self.get_cell_mut(x + dx, y + dy) {
+                    if neighbor.state != CellState::Flagged {
+                        neighbor.state = CellState::Revealed;
+                        if neighbor.kind == CellKind::Mine {
+                            ret = Some(GameState::Lost);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+    fn handle_mouse_left(&mut self, event: event::MouseEvent) -> Option<GameState> {
         let (board_start_x, board_start_y) = self.get_board_start_pos();
         let cell_x = (event.column as isize - board_start_x as isize) / 2;
         let cell_y = event.row as isize - board_start_y as isize;
+        if self.mines_placed == false {
+            self.place_mines(cell_x, cell_y);
+            self.mines_placed = true;
+        }
         if let Some(cell) = self.get_cell_mut(cell_x, cell_y) {
-            cell.state = match cell.state {
-                CellState::Hidden => CellState::Revealed,
-                _ => cell.state, // Do nothing if it's already revealed or flagged
+            match cell.state {
+                CellState::Hidden => {
+                    // Only reveal if the cell is hidden
+                    if let Some(game_state_change) = self.reveal_adjacent_empty(cell_x, cell_y) {
+                        return Some(game_state_change);
+                    };
+                }
+                CellState::Revealed => {
+                    // If already revealed, reveal adjacent non-flagged cells if it's a number
+                    if let CellKind::Number(n) = cell.kind {
+                        if n > 0 {
+                            if let Some(game_state_change) = self.reveal_non_flagged(cell_x, cell_y)
+                            {
+                                return Some(game_state_change);
+                            };
+                        }
+                    }
+                }
+                _ => (), // Do nothing if it's already revealed or flagged
             };
         } else {
             write!(stdout(), "Mouse click out of bounds!").expect("!");
         }
+        None
     }
     fn handle_mouse_right(&mut self, event: event::MouseEvent) {
         let (board_start_x, board_start_y) = self.get_board_start_pos();
@@ -192,6 +250,41 @@ fn cleanup_terminal(mut stdout: &Stdout) -> Result<(), std::io::Error> {
     return Ok(());
 }
 
+fn render_game_board(board: &Board, stdout: &mut Stdout) -> anyhow::Result<()> {
+    let (board_start_x, board_start_y) = board.get_board_start_pos();
+    queue!(
+        stdout,
+        Clear(terminal::ClearType::All),
+        MoveTo(board_start_x, board_start_y)
+    )?;
+    let mut i = 0;
+    for y in 0..board.width {
+        for x in 0..board.height as isize {
+            if let Some(cell) = board.get_cell(x as isize, y as isize) {
+                let (symbol, color) = match cell.state {
+                    CellState::Hidden => ("■", Color::DarkGrey), // Hidden cell
+                    CellState::Revealed => match cell.kind {
+                        CellKind::Mine => ("💣", Color::Black),      // Mine
+                        CellKind::Number(0) => ("  ", Color::Black), // Empty cell
+                        CellKind::Number(n) => (&n.to_string()[..], Color::Blue),
+                    },
+                    CellState::Flagged => ("F", Color::DarkRed), // Flagged cell
+                };
+                let symbol = if symbol == "💣" {
+                    format!("{:>1}", symbol)
+                } else {
+                    format!("{:^2}", symbol)
+                };
+                queue!(stdout, SetForegroundColor(color), Print(symbol))?;
+            }
+        }
+        i += 1;
+        queue!(stdout, MoveTo(board_start_x, i + board_start_y))?;
+    }
+    stdout.flush()?;
+    Ok(())
+}
+
 fn should_exit(event: &Event) -> Result<bool, std::io::Error> {
     if let Event::Key(key_event) = event.to_owned() {
         if key_event.code == KeyCode::Esc
@@ -210,8 +303,8 @@ fn main() -> Result<(), anyhow::Error> {
     setup_terminal(&stdout)?;
     set_styles(&stdout)?;
     let mut board = Board::new(10, 10);
-    board.place_mines(8);
-    board.render_game_board(&mut stdout)?;
+    render_game_board(&board, &mut stdout)?;
+    let mut game_state = GameState::Ongoing;
 
     'drawing: loop {
         let event = event::read()?;
@@ -228,7 +321,9 @@ fn main() -> Result<(), anyhow::Error> {
                     stdout.execute(MoveTo(event.column, event.row))?;
                 }
                 event::MouseEventKind::Down(MouseButton::Left) => {
-                    board.handle_mouse_left(event);
+                    if let Some(game_state_change) = board.handle_mouse_left(event) {
+                        game_state = game_state_change;
+                    };
                 }
 
                 event::MouseEventKind::Down(MouseButton::Right) => {
@@ -241,7 +336,21 @@ fn main() -> Result<(), anyhow::Error> {
             }
         }
 
-        board.render_game_board(&mut stdout)?;
+        if let GameState::Lost = game_state {
+            // Reveal all mines
+            for cell in board.grid.iter_mut() {
+                if cell.kind == CellKind::Mine {
+                    cell.state = CellState::Revealed;
+                }
+            }
+            render_game_board(&board, &mut stdout)?;
+            cleanup_terminal(&stdout)?;
+            println!("You hit a mine! Game Over!");
+            std::thread::sleep(std::time::Duration::from_secs(3));
+            return Ok(());
+        }
+
+        render_game_board(&board, &mut stdout)?;
     }
     cleanup_terminal(&stdout)?;
     Ok(())
